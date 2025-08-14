01e7a19a8db6cf9f03c6c366dd2ba5f3610c4c96
Shanna Dugho <168612597+shnnmdnll@users.noreply.github.com>
2025-08-14 13:31:49 +0800

    GitUserInputAct4

---
+++ Act4.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+void main() {
+    print("Enter your name");
+    String? name = stdin.readLineSync();
+    print("your name is ${name}");
+
+    print("Enter your section");
+    String? section = stdin.readLineSync();
+    print("your name is ${section}");
+
+    print("Enter your course");
+    String? course = stdin.readLineSync();
+    print("your name is ${course}");
+
+    print("Enter your year level");
+    String? yearlevel = stdin.readLineSync();
+    print("your name is ${yearlevel}");
+
+    print("Enter your subject");
+    String? subject = stdin.readLineSync();
+    print("your name is ${subject}");
+   
+   
+   
+}
